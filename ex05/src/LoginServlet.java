a2fc693123d4d2720ba1eee0a865db70a2ee800d
Hemavardhini27 <hemapj2007@gmail.com>
2026-09-24 14:14:03 +0530

    Add files via upload

---
+++ ex05/src/LoginServlet.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<h2>Login Page</h2>");
+        out.println("<form method='post' action='LoginServlet'>");
+        out.println("Username: <input type='text' name='username'><br><br>");
+        out.println("Password: <input type='password' name='password'><br><br>");
+        out.println("<input type='submit' value='Login'>");
+        out.println("</form>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String uname = request.getParameter("username");
+        String pwd = request.getParameter("password");
+
+        if("admin".equals(uname) && "12345".equals(pwd)) {
+            out.println("<h3>Login Successful! Welcome, " + uname + ".</h3>");
+        } else {
+            out.println("<h3>Login Failed! Invalid username or password.</h3>");
+            out.println("<a href='login.html'>Try Again</a>");
+        }
+    }
+}
